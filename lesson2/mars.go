80f4344738b96d1feffe1cfa1f95c5381c540446
Aldo Espinosa <aldoespinosaperez1@gmail.com>
2025-05-16 01:47:19 -0600

    Print format

---
+++ lesson2/mars.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("My weight on the surface of Mars is %v lbs,", 84*0.3783)
+	fmt.Printf(" and I would be %v years old.\n", 26*365/687)
+	fmt.Printf("%-15v $%4v\n", "SpaceX", 94)
+	fmt.Printf("%-15v $%4v\n", "Virgin Galactic", 100)
+}
