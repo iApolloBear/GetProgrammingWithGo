4d710ff250c6ced9f936d05b154ac214cae5faf6
Aldo Espinosa <aldoespinosaperez1@gmail.com>
2025-05-25 00:16:54 -0600

    Chess challenge completed

--- lesson16/chess.go
+++ lesson16/chess.go
@@ -2,15 +2,42 @@ package main
 
 import "fmt"
 
-func main() {
-	var board [8][8]string
+const line = "=========="
 
-	board[0][0] = "r"
-	board[0][7] = "r"
+func getRow(row int, column int) rune {
+	whitePieces := [8]rune{9820, 9822, 9821, 9819, 9818, 9821, 9822, 9820}
+	blackPieces := [8]rune{9814, 9816, 9815, 9813, 9812, 9815, 9816, 9814}
+	const whitePawn = 9823
+	const blackPawn = 9817
 
-	for column := range board[1] {
-		board[1][column] = "p"
+	switch row {
+	case 0:
+		return blackPieces[column]
+	case 1:
+		return blackPawn
+	case 6:
+		return whitePawn
+	case 7:
+		return whitePieces[column]
+	default:
+		return 20
 	}
+}
 
-	fmt.Print(board)
+func drawBoard() {
+	var board [8][8]rune
+
+	fmt.Println(line)
+	for row := range board {
+		fmt.Print("|")
+		for column := range board[row] {
+			fmt.Printf("%c", getRow(row, column))
+		}
+		fmt.Print("|\n")
+	}
+	fmt.Println(line)
+}
+
+func main() {
+	drawBoard()
 }
