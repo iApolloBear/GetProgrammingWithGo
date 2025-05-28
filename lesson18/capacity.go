347a4ea6d5d731410c444bd6e4e87128acf5e8ad
Aldo Espinosa <aldoespinosaperez1@gmail.com>
2025-05-28 15:14:10 -0600

    Capacity challenge completed

--- lesson18/capacity.go
+++ lesson18/capacity.go
@@ -9,6 +9,7 @@ func main() {
 	for i := range 100 {
 		slc = append(slc, i)
 		if capacity != cap(slc) {
+			fmt.Printf("Last element before capacity is recalculated %v\n", i)
 			capacity = cap(slc)
 			fmt.Println(capacity)
 		}
