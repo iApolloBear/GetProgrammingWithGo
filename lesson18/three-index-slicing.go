503a9da8ee60d2c167be4497aa22db77419552b8
Aldo Espinosa <aldoespinosaperez1@gmail.com>
2025-05-28 14:53:33 -0600

    Three index in slices

---
+++ lesson18/three-index-slicing.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	planets := []string{
+		"Mercury", "Venus", "Earth", "Mars",
+		"Jupiter", "Saturn", "Uranus", "Neptune",
+	}
+
+	terrestrial := planets[0:4:4]
+	worlds := append(terrestrial, "Ceres")
+	fmt.Println(planets)
+	fmt.Println(worlds)
+}
